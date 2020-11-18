22f00bf62ab35e1f680623123bdd0d3fba26788b
Erick Recher <javascriptforlife@gmail.com>
2020-11-17 18:36:04 -0800

    message

--- 03_color_search/scripts/app.js
+++ 03_color_search/scripts/app.js
@@ -1,7 +1,10 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
-
-  // code in here
-
+// $(document).ready(function(){
+//   $("#color-search-from").on("submit", function(event){
+//     event.preventDefault();
+//     var formData = $(this).serialize();
+//     console.log(formData);
+//   });
+  
 });
